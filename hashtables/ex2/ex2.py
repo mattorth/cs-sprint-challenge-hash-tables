8c1a67a843da6a3ce8ec2fdc11b1f24f2d8fbee5
mattorth <orth.mj@gmail.com>
2020-11-08 21:23:35 -0600

    ex2 done

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -5,10 +5,30 @@ class Ticket:
         self.destination = destination
 
 
+# inputs: tickets ( source and destination) and length
+# output: array of destinations in order
+# destination must match following source
+# solution should run in linear time
+
+
+
 def reconstruct_trip(tickets, length):
     """
     YOUR CODE HERE
     """
     # Your code here
+    log = {}
+    route = [None] * length
+    for ticket in tickets:
+        log[ticket.source] = ticket.destination
 
+    source = "NONE"
+    destination = ""
+    idx = 0
+    while destination != "NONE":
+        route[idx] = log[source]
+        source = log[source]
+        destination = log[source]
+        idx += 1
+    route[idx] = destination
     return route
